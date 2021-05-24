15fb849c12406de07394a207f8ae2892212b7b58
egrosscurt <84039948+egrosscurt@users.noreply.github.com>
2021-05-24 14:22:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Hello Winc Academy");
+let name = 'Esther';
+console.log('Esther');
+//let if = 5; Kan niet want if is een reserved keyword
+console.log(8);
+console.log("44");
+console.log(33);
+console.log(60);
+console.log("number");
+let age = "negenentwintig";
+console.log("string");
